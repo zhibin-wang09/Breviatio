9027574e059f46ad98d42d4fc5d910a2fb3bc30b
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2025-03-27 16:33:39 -0400

    Able to auth using InstalledAppFlow

--- server/main.py
+++ server/main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
 from api import mail
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 app = FastAPI()
+SCOPES = [
+    'openid',
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/userinfo.email',
+    'https://www.googleapis.com/auth/userinfo.profile'
+]
 
 @app.get("/")
 def read_root():
     return {"hello": "world"}
 
-@app.get("/auth/google")
+@app.get("/authorize")
 def auth():
-    pass
+    flow = InstalledAppFlow.from_client_secrets_file('./doc/credentials.json',
+                                                     scopes=SCOPES)
+    flow.redirect_uri = "http://localhost"
+    credentials = flow.run_local_server(open_browser=True)
+    print(f"CREDENTIALS: {credentials}")
+    return {"success"}
+    
 
 @app.get("/messages/{userId}")
 def getMessagesFrom(userId: str):
