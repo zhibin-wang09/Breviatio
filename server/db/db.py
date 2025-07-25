0af6aeea3b4ee15a3d9b2cac53d3930e7111cd8b
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2025-07-24 22:31:01 -0400

    Get called on server start

--- server/db/db.py
+++ server/db/db.py
@@ -1,21 +1,20 @@
 from sqlmodel import create_engine, SQLModel
-from .models import *
+from server.models import *
 import os
 
 database_url = os.getenv('DB_CONNECTION_STRING')
 
-# engine is an object that handles communication with the database
-# echo prints out all statements executed in SQL, helps us debug and understand what's happening
+# # engine is an object that handles communication with the database
+# # echo prints out all statements executed in SQL, helps us debug and understand what's happening
 engine = create_engine(database_url, echo=True)
 
 def create_db_and_tables():
     # SQLModel class has attribute metadata where it contains
     # all the informations/table registered. For for instance
-    # User_Credentials table inherits from SQLModel and has table
+    # User table inherits from SQLModel and has table
     # set to true. This table is then registered in SQLModel.metadata
     # so using create_all method we can create the database and all tables
     # registered in the metadata object
     SQLModel.metadata.create_all(engine)
 
-if __name__ == '__main__':
-    create_db_and_tables()
+create_db_and_tables()
