a086d78aa7384652f1fcc9fea12ef6df59712542
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2025-07-25 16:48:01 -0400

    Able to parse emails to retrieve bodies and headers then load them into Email object

--- server/models/email.py
+++ server/models/email.py
@@ -1,10 +1,13 @@
 from typing import Optional
 from sqlmodel import Field, SQLModel
+import json
 
-class Email(SQLModel, table=True):
-    id: Optional[int] = Field(default = None, primary_key = True)
-    source: str
-    to: str
-    subject: str
-    body: str
-    label: str
+class Email():
+    def __init__(self, date: str, mimeType: str, source: str, to: str, subject: str, snippet: str, body: list[str]):
+        self.mimeType = mimeType
+        self.source = source
+        self.to = to
+        self.subject = subject
+        self.body = body
+        self.snippet = snippet
+        self.date = date
